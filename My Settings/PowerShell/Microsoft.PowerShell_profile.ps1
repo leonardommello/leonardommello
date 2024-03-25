3d3d95b79a23036e27e38403e43a429e0ad7cdb5
Leonardo Medeiros de Mello Junior <leomedeirosdemello@gmail.com>
2024-03-25 20:15:25 -0300

    Add files via upload

--- My Settings/PowerShell/Microsoft.PowerShell_profile.ps1
+++ My Settings/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -1,20 +1,21 @@
-$modules = @(
-    @{Name = 'PowerShellGet'; AllowPrerelease = $true },
-    @{Name = 'posh-git'; AllowPrerelease = $true },
-    @{Name = 'oh-my-posh'; AllowPrerelease = $true },
-    @{Name = 'Terminal-Icons'; AllowPrerelease = $true },
-    @{Name = 'PSReadLine'; AllowPrerelease = $true }
+$listPlugins = @(
+    "PowerShellGet"
+    "posh-git"
+    "oh-my-posh"
+    "Terminal-Icons"
+    "PSReadLine"
+    "psInlineProgress"
 )
 
-foreach ($module in $modules) {
-    if (!(Get-Module -ListAvailable -Name $module.Name)) {
-        Install-Module @module -Force
+foreach ($modules in $listPlugins) {
+    if (!(Get-Module -ListAvailable -Name $modules)) {
+        Install-Module $modules -Force
     }
 }
 
-Import-Module -Name Terminal-Icons, posh-git, PSReadLine, PowerShellGet
+Import-Module -Name Terminal-Icons, posh-git, PSReadLine, PowerShellGet, psInlineProgress, PSWindowsUpdate
 
-if ($host.Name -eq 'ConsoleHost') {
+if ($host.Name -eq "ConsoleHost") {
     Import-Module -Name PSReadLine
     Set-PSReadlineKeyHandler -Key Tab -Function MenuComplete
     Set-PSReadLineKeyHandler -Key UpArrow -Function HistorySearchBackward
@@ -26,4 +27,8 @@ Set-Alias -Name vim -Value code
 $env:VIRTUAL_ENV_DISABLE_PROMPT = 1
 $env:POSH_GIT_ENABLED = $true
 oh-my-posh init pwsh --config "~\Documents\PowerShell\bubbles.omp.json" | Invoke-Expression
-Clear-Host
+
+#34de4b3d-13a8-4540-b76d-b9e8d3851756 PowerToys CommandNotFound module
+
+Import-Module "C:\Program Files\PowerToys\WinUI3Apps\..\WinGetCommandNotFound.psd1"
+#34de4b3d-13a8-4540-b76d-b9e8d3851756
